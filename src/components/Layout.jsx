b6ed8118bf6905c008ccbf4a611d79023b853626
Hanu Asfaw <hanuasfaw+github@icloud.com>
2022-03-20 03:31:47 -0400

    close #3

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,13 @@
-import * as React from "react"
+import * as React from "react";
 
 const Layout = ({ children }) => {
 	return (
 		<div className="bg-white">
-			<div className="mx-auto md:w-50 container pb-16 pt-10 px-4">
-				<div className="mt-20">
-					{children}
-				</div>
+			<div className="mx-auto md:w-50 container pb-16 pt-14 px-4">
+				<div>{children}</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Layout
+export default Layout;
